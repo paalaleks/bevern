3f6c404c20fb29dbb50a7c130434fb7f32da16a9
paalaleks <paalaleks@hotmail.com>
2023-09-26 07:51:11 +0200

    commit_18

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,10 +2,10 @@ import Link from 'next/link';
 import { RxNotionLogo } from 'react-icons/rx';
 export default function Footer() {
   return (
-    <div className="text-xs flex flex-col justify-end items-end absolute right-4 bottom-4 opacity-50 hover:opacity-100">
+    <div className="text-xs flex flex-col justify-end items-end pb-4 mr-8 opacity-50 hover:opacity-100">
       <Link
         href={`https://www.paalsportfolio.no`}
-        className="inline-flex items-center"
+        className="inline-flex items-center z-20 relative"
       >
         By Paal A, using <RxNotionLogo className="ml-1" />
       </Link>
